6262ab652c7e6e29270b65e2010a278843d1be74
Anne-Lise <annelisecoco.21@gmail.com>
2024-12-19 10:15:13 +0100

    Initial commit for Vapor project

--- Sources/App/Models/UserModel.swift
+++ Sources/App/Models/UserModel.swift
@@ -6,19 +6,3 @@
 //
 import Vapor
 import Fluent
-
-final class UserModel: Model, @unchecked Sendable {
-    static let databaseTableName = "user"
-    var id: UUID?
-    var name: String
-    var email: String
-    var password: String
-    var height: Double?
-    var weight: Double?
-    var oj
-    
-    init(name: String, email: String, password: String) {
-        self.name = name
-        self.email = email
-    }
-}
